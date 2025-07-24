4ed15d8e2f788017dce49c5acd69ee665b720808
Josie Leye <jiley1@student.monash.edu>
2025-07-24 12:01:30 +1000

    Fixed background position

--- src/components/baselayout/BaseLayout.js
+++ src/components/baselayout/BaseLayout.js
@@ -8,6 +8,30 @@ position: relative;
 min-height: 100vh;
 display: flex;
 flex-direction: column;
+
+/* Sectioned background effect*/
+&::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    width: 30%;
+    min-width: 320px;
+    background: var(--color-background-gradient);
+    z-index: -1;
+}
+
+@media (max-width: 768px) {
+    &::before {
+      top: 0;
+      left: 0;
+      right: 0;
+      height: 30%;
+      min-height: 350px;
+      width: 100%;
+    }
+}
 `
 const Main = styled.main`
 flex: 1;
